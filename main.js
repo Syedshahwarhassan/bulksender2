c97e7ead4b06fe9cace524acb4e3fcbc08e1ac98
syedshahwarhassan <syedshahwarhassan87@gmail.com>
2024-12-17 17:50:54 +0500

    entry

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express=require('express')
-
+const nodemailer=require('nodemailer')
 const app=express();
-
+require("dotenv").config();
 
 app.listen(4600,()=>console.log('Server is running on port 4600'));
 app.set('view engine', 'ejs');
@@ -9,16 +9,54 @@ app.set('views', __dirname + '/views');
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
-let employees=[
-  {id:1, name:'John Doe', department:'Sales', salary:5000},
-  {id:2, name:'Jane Smith', department:'Marketing', salary:6000},
-  {id:3, name:'Michael Johnson', department:'Finance', salary:7000}]
+let emaillist=[
+  "contact@digitalsohail.com",
+  "info@aamax.co",
+  "info@avanzasolutions.com",
+  "info@itgenesis.net",
+  "contact@arbisoft.com",
+   "hi@techabout.com",
+  "info@northbaysolutions.com",
+  "sales@ranatechnologies.com",
+"  info@pixelpk.com",
+  "contact@bittechnologies.com",
+]
   app.get('/',(req,res)=>{
     res.render('index')
   })
-  app.get('/add',(req,res)=>{
-    res.send(employees)
-  })
+  app.post('/send', async (req, res) => {
+    const { subject,content } = req.body; 
+    if(!subject||!content){
+      return res.status(400).send('Please provide subject and content.');
+    }
+    const transport=nodemailer.createTransport({
+      service: 'gmail',
+      port:487,
+      auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASS,
+      }
+    })
+  
+   emaillist.forEach((c,i)=>{
+    const mailOptions={
+      from: process.env.EMAIL_ADDRESS,
+      to: c,
+      subject: subject,
+      html: content
+    }
+    transport.sendMail(mailOptions, (error, info) => {
+      if(error) {
+        console.log('Error occurred:', error);
+        res.status(500).send('Email sending failed.');
+      } else {
+        console.log('Email sent successfully:', info.response);
+        res.status(200).send('Email sent successfully.');
+      }
+    });
+   })
+ 
+});
 
 
 
